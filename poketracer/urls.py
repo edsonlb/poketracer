2d960bc6bed26476f582c99a48c26371a284e794
Lucas Castejon <lucas@celuladigital.com.br>
2013-10-28 12:11:17 -0200

    Criando página Donate

--- poketracer/urls.py
+++ poketracer/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
 
 	url(r'^poketracer/$', TemplateView.as_view(template_name="index.html")),
 	url(r'^poketracer/about/$', TemplateView.as_view(template_name="about.html")),
+	url(r'^poketracer/donate/$', TemplateView.as_view(template_name="donate.html")),
 	#url(r'^admin/funcionario/', include('sistema.urls_funcionario')),
 )
